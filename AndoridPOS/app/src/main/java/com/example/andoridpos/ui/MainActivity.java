835e6a3c306be1f594d7f81948750b9aa4a2e709
MinnKhantKyaww <minkhant199701@gmail.com>
2019-08-13 07:38:10 +0630

    Updated

--- AndoridPOS/app/src/main/java/com/example/andoridpos/ui/MainActivity.java
+++ AndoridPOS/app/src/main/java/com/example/andoridpos/ui/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.andoridpos.ui;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.andoridpos.R;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerSideNav drawerSideNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +30,56 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerSideNav = findViewById(R.id.drawer_layout);
+
+        //DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.navigation_view);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerSideNav, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerSideNav.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
         NavigationUI.setupWithNavController(navigationView, Navigation.findNavController(this, R.id.my_nav_host_fragment));
+
+        drawerSideNav.setViewScale(Gravity.START, 0.9f);
+        drawerSideNav.setRadius(Gravity.START, 35);
+        drawerSideNav.setViewElevation(Gravity.START, 20);
+        drawerSideNav.setScrimColor(Color.parseColor("#03A9F4"));
     }
+
+    @Override
+    public void onBackPressed() {
+
+        if ((drawerSideNav.isDrawerOpen(GravityCompat.START))) {
+            drawerSideNav.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        drawerSideNav.closeDrawer(GravityCompat.START);
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_res, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+   /* @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case  R.id.actio
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
 }
